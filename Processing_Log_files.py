5fe3ee6f5291da74124dd47f3505346f71d02469
davideliason <davethemaker@gmail.com>
2022-05-24 01:09:24 -0700

    file with sample function and test file that imports that function

---
+++ Processing_Log_files.py
@@ -0,0 +1,7 @@
+#!/usr//bin/env python3
+import sys
+
+logfile=sys.argv[1]
+with open(logfile.txt) as f:
+	for line in f:
+		print(line.strip())
---
+++ Testing/Unit Tests/basic_function_to_export_test.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+from basic_function_to_export import rearrange_name
