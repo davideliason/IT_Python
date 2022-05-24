92666ff4a350e861d4fccbbf633cb33991c6c209
davideliason <davethemaker@gmail.com>
2022-05-24 01:22:51 -0700

    build custom test class inheriting from unittest TestCase

--- Testing/Unit Tests/basic_function_to_export_test.py
+++ Testing/Unit Tests/basic_function_to_export_test.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 
 from basic_function_to_export import rearrange_name
+import unittest  # import test module for testing
+
+# The unittest module providesa TestCase class with a number of testing methods built-in
+
+class TestRearrange(unittest.TestCase): # this is our custom class that inherits from TestCase
+	
