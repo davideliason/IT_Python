85eff8efc9ba84f9340583cfb1ae4e29c9ffcb10
davideliason <davethemaker@gmail.com>
2022-05-24 01:34:38 -0700

    successful unit test on rearrange_name function within basic_function_to_export

--- Testing/Unit Tests/basic_function_to_export_test.py
+++ Testing/Unit Tests/basic_function_to_export_test.py
@@ -6,4 +6,13 @@ import unittest  # import test module for testing
 # The unittest module providesa TestCase class with a number of testing methods built-in
 
 class TestRearrange(unittest.TestCase): # this is our custom class that inherits from TestCase
-	
+	def test_basic(self):
+		testcase = "Smith, John"
+		expected = "John Smith"
+		self.assertEqual(rearrange_name(testcase), expected)
+
+#unittest.main() to run the test
+unittest.main()
+
+
+# in terminal, "chmod +x basic_function_to_export.py"
